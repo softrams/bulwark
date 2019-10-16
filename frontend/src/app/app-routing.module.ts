d68310e28102b15fe3553eb17a294c774e61fa27
Alejandro Saenz <alejandro.saenz@softrams.com>
2019-10-16 15:26:22 -0400

    added navbar and added router

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from '../app/dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard/organization',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard/organization',
+    component: DashboardComponent
+  },
+  {
+    path: 'dashboard/organization/:id',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
