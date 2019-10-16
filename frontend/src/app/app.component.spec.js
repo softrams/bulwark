fd2b0f18aa12cf58e42f67be2dc81a96440cb593
Alejandro Saenz <alejandro.saenz@softrams.com>
2019-10-16 09:32:10 -0400

    initial commit for angular

---
+++ frontend/src/app/app.component.spec.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var testing_1 = require("@angular/core/testing");
+var testing_2 = require("@angular/router/testing");
+var app_component_1 = require("./app.component");
+describe('AppComponent', function () {
+    beforeEach(testing_1.async(function () {
+        testing_1.TestBed.configureTestingModule({
+            imports: [
+                testing_2.RouterTestingModule
+            ],
+            declarations: [
+                app_component_1.AppComponent
+            ],
+        }).compileComponents();
+    }));
+    it('should create the app', function () {
+        var fixture = testing_1.TestBed.createComponent(app_component_1.AppComponent);
+        var app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    });
+    it("should have as title 'frontend'", function () {
+        var fixture = testing_1.TestBed.createComponent(app_component_1.AppComponent);
+        var app = fixture.debugElement.componentInstance;
+        expect(app.title).toEqual('frontend');
+    });
+    it('should render title in a h1 tag', function () {
+        var fixture = testing_1.TestBed.createComponent(app_component_1.AppComponent);
+        fixture.detectChanges();
+        var compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('h1').textContent).toContain('Welcome to frontend!');
+    });
+});
