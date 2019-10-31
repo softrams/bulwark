02626e9fd3f04b2f26adffb4204bf8cf33b8910b
Bill Jones <bill.jones@softrams.com>
2019-10-31 12:52:06 -0400

    SEC-150 Resource -> Vulnerability Link

--- src/entity/Resource.ts
+++ src/entity/Resource.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { Vulnerability } from './Vulnerability';
 
 @Entity()
