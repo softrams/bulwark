c8ab99274859d9287ff21f9e82416dd2ef1d73db
Darrell Richards <hello@darrellrichards.com>
2020-07-08 21:02:06 -0400

    fix(jwt.spec.ts): removed need for process.env
    Also fixed issues with TSLint

--- src/middleware/jwt.spec.ts
+++ src/middleware/jwt.spec.ts
@@ -1,4 +1,3 @@
-require('dotenv').config();
 import jwt = require('jsonwebtoken');
 const { checkToken, checkRefreshToken } = require('./jwt.middleware');
 
@@ -11,7 +10,7 @@ describe('jwt.middleware.ts', () => {
             },
             body: {},
             user: Function,
-        };  
+        };
         req.user = jest.fn().mockReturnValue(req);
         return req;
     };
@@ -25,17 +24,18 @@ describe('jwt.middleware.ts', () => {
         res.json = jest.fn().mockReturnValue(res);
         return res;
     };
-    let token: string, refreshToken: string;
+    let token: string;
+    let refreshToken: string;
     beforeAll(() => {
-        token = jwt.sign({ userId: '2222' }, process.env.JWT_KEY, { expiresIn: '15m' });
-        refreshToken = jwt.sign({ userId: '2222' }, process.env.JWT_REFRESH_KEY, { expiresIn: '8h' });
+        token = jwt.sign({ userId: '2222' }, 'testJWT', { expiresIn: '15m' });
+        refreshToken = jwt.sign({ userId: '2222' }, 'testJWT', { expiresIn: '8h' });
     });
 
     test('running checkToken should return a 401 with Authorization', async () => {
         const req = mockRequest();
         const res = mockResponse();
         await checkToken(req, res, null);
-        expect(res.status).toHaveBeenCalledWith(401);  
+        expect(res.status).toHaveBeenCalledWith(401);
     });
 
     test('running checkToken should return a req.user', async () => {
@@ -43,7 +43,7 @@ describe('jwt.middleware.ts', () => {
         const res = mockResponse();
         req.headers.authorization = token;
         await checkToken(req, res, jest.fn());
-        expect(req.user).toBe('2222');  
+        expect(req.user).toBe('2222');
     });
 
     test('running checkToken should return a 401 without Authorization', async () => {
@@ -51,7 +51,7 @@ describe('jwt.middleware.ts', () => {
         delete(req.headers.authorization);
         const res = mockResponse();
         await checkToken(req, res, null);
-        expect(res.status).toHaveBeenCalledWith(401);  
+        expect(res.status).toHaveBeenCalledWith(401);
     });
 
     test('running checkRefreshToken should return a 401 with refreshToken', async () => {
@@ -61,22 +61,22 @@ describe('jwt.middleware.ts', () => {
             'refreshToken': 'FakeJWT',
         };
         await checkRefreshToken(req, res, null);
-        expect(res.status).toHaveBeenCalledWith(401);  
+        expect(res.status).toHaveBeenCalledWith(401);
     });
     test('running checkRefreshToken should return a 401 without refreshToken', async () => {
         const req = mockRequest();
         const res = mockResponse();
         await checkRefreshToken(req, res, null);
-        expect(res.status).toHaveBeenCalledWith(401);  
+        expect(res.status).toHaveBeenCalledWith(401);
     });
 
     test('running checkRefreshToken should return a req.user', async () => {
         const req = mockRequest();
         const res = mockResponse();
         req.body = {
-            'refreshToken': refreshToken,
+            refreshToken,
         };
         await checkRefreshToken(req, res, jest.fn());
-        expect(req.user).toBe('2222');  
+        expect(req.user).toBe('2222');
     });
 });
