afba021856cabfde61d7f833998b9be138dcabb2
Alejandro Saenz <alejandro.saenz@softrams.com>
2020-08-17 16:13:02 -0400

    fix(ormconfig): updated ormconfig migrations array
    Fixed migrations bug where the server would not start due to misconfigured migrations
    
    fix #208

--- ormconfig.js
+++ ormconfig.js
@@ -6,7 +6,7 @@ module.exports = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: [__dirname + '/dist/entity/*.js'],
-  migrations: ['migration/*.ts'],
+  migrations: ['/dist/migration/*.js'],
   cli: {
     migrationsDir: 'migration'
   },
