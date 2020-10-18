2af9e0eb51cbeb93ca07cc15abcbf10f98996223
Joel Lau <joel.lau@protonmail.com>
2020-10-18 21:32:46 +0800

    refactor(navbar): refactor navbar -> primeng menubar
    replace custom <nav> element with primeng menubar component
    
    fix #320

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { LoaderService } from '../loader.service';
 import { AuthService } from '../auth.service';
 import { GlobalManagerService } from '../global-manager.service';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
@@ -11,14 +12,45 @@ import { GlobalManagerService } from '../global-manager.service';
 })
 export class NavbarComponent implements OnInit {
   loggedIn$ = this.globalManager.loggedIn$;
+  menuItems: MenuItem[];
 
   constructor(
     public loaderService: LoaderService,
     public authService: AuthService,
     private globalManager: GlobalManagerService
-  ) {}
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initMenuItems();
+  }
+
+  initMenuItems() {
+    this.menuItems = [
+      {
+        label: 'Profile',
+        icon: 'pi pi-fw pi-user',
+        routerLink: ['user/profile']
+      },
+      {
+        label: 'Invite User',
+        icon: 'pi pi-fw pi-plus',
+        routerLink: ['invite']
+      },
+      {
+        label: 'Settings',
+        icon: 'pi pi-fw pi-sliders-h',
+        routerLink: ['settings']
+      },
+      {
+        separator: true
+      },
+      {
+        label: 'Logout',
+        icon: 'pi pi-fw pi-power-off',
+        command: this.logout
+      }
+    ];
+  }
 
   logout() {
     this.authService.logout();
