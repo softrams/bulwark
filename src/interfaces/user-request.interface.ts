54e46476441ce6c0cde3e89b80606026694f484a
Alejandro Saenz <alejandro.saenz@softrams.com>
2021-01-09 13:37:40 -0500

    feat(jwt.middleware.ts): integrate roles into jwt middleware
    Implemented and integrated function to determine user organizations and assets by team
    
    feat #484

--- src/interfaces/user-request.interface.ts
+++ src/interfaces/user-request.interface.ts
@@ -1,9 +1,13 @@
 import { Request } from 'express';
 import { File } from '../entity/File';
+import { TeamInfo } from './team-info.interface';
 
 export interface UserRequest extends Request {
   user: string;
+  organization: number[];
+  assets: number[];
   fileExtError: string;
   file: File;
   files: File[];
+  isAdmin: boolean;
 }
--- src/middleware/jwt.middleware.ts
+++ src/middleware/jwt.middleware.ts
@@ -1,22 +1,26 @@
 import jwt = require('jsonwebtoken');
+import { Asset } from '../entity/Asset';
+import { Organization } from '../entity/Organization';
+import { UserRequest } from '../interfaces/user-request.interface';
 import { getConnection } from 'typeorm';
-import { Team } from '../entity/Team';
 import { User } from '../entity/User';
 import { ROLE } from '../enums/roles-enum';
+import { Response } from 'express';
 
 /**
  * @description Checks for valid token before API logic
  * @param {Request} req
  * @param {Response} res
  */
-export const checkToken = (req, res, next) => {
+export const checkToken = (req: UserRequest, res: Response, next) => {
   const token = req.headers.authorization; // Express headers are auto converted to lowercase
   if (token) {
-    jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+    jwt.verify(token, process.env.JWT_KEY, async (err, decoded) => {
       if (err) {
         return res.status(401).json('Authorization token is not valid');
       } else {
         req.user = decoded.userId;
+        await fetchRoles(req);
         next();
       }
     });
@@ -62,5 +66,39 @@ export const isAdmin = async (req, res, next) => {
     next();
   } else {
     return res.status(403).json('Authorization is required');
+  }
+};
+
+// Determine if the user is an Administrator
+// If the user is an administrator, return all organizations and assets
+// Else return only the organization and assets associated to team
+export const fetchRoles = async (req: UserRequest) => {
+  const user = await getConnection()
+    .getRepository(User)
+    .findOne(req.user, {
+      join: {
+        alias: 'user',
+        leftJoinAndSelect: {
+          teams: 'user.teams',
+          organization: 'teams.organization',
+          assets: 'teams.assets',
+        },
+      },
+    });
+  const isAdmin = user.teams.some((team) => team.role === ROLE.ADMIN);
+  if (isAdmin) {
+    const orgs = await getConnection().getRepository(Organization).find({});
+    const assets = await getConnection().getRepository(Asset).find({});
+    req.organization = orgs.map((org) => org.id);
+    req.assets = assets.map((asset) => asset.id);
+  } else {
+    req.organization = user.teams.map((team) => team.organization.id);
+    req.assets = [];
+    for (const team of user.teams) {
+      const assets = team.assets.map((asset) => asset.id);
+      for (const asset of assets) {
+        req.assets.push(asset);
+      }
+    }
   }
 };
