801c9a3742ffb515a90880147d2d1ab97c67774f
waterweasel4 <ryan.mathus@ip-10-3-252-99.ec2.internal>
2021-01-26 16:27:43 -0500

    fix(fix csp errors): cSP errors were generated in console
    CSP errors were generated in the console when the URL didn't match what the /api/login said.
    
    fix565

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,9 @@ if (fs.existsSync(path.join(__dirname, '../.env'))) {
   }
 } else {
   // tslint:disable-next-line: no-console
-  console.info('A .env file was not found. Attempting to fetch values from existing environment variables.');
+  console.info(
+    'A .env file was not found. Attempting to fetch values from existing environment variables.'
+  );
 }
 import * as bodyParser from 'body-parser';
 import { createConnection } from 'typeorm';
@@ -36,20 +38,11 @@ import * as configController from './routes/config.controller';
 const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
-app.use(helmet());
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self' blob:", 'stackpath.bootstrapcdn.com'],
-      scriptSrc: ["'self'", 'code.jquery.com', 'stackpath.bootstrapcdn.com'],
-      styleSrc: ["'self'", 'stackpath.bootstrapcdn.com', "'unsafe-inline'"]
-    }
-  })
-);
+
 app.use(cors());
 app.use(
   express.static(path.join(__dirname, '../frontend/dist/frontend'), {
-    etag: false
+    etag: false,
   })
 );
 app.use(bodyParser.json({ limit: '2mb' }));
@@ -59,7 +52,26 @@ const serverIpAddress = process.env.IP || '127.0.0.1';
 app.set('port', serverPort);
 app.set('serverIpAddress', serverIpAddress);
 // tslint:disable-next-line: no-console
-app.listen(serverPort, () => console.info(`Server running on ${serverIpAddress}:${serverPort}`));
+app.listen(serverPort, () =>
+  console.info(`Server running on ${serverIpAddress}:${serverPort}`)
+);
+
+app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self' blob:", 'stackpath.bootstrapcdn.com'],
+      scriptSrc: [
+        "'self'",
+        'code.jquery.com',
+        'stackpath.bootstrapcdn.com',
+        '${serverIpAddress}',
+      ],
+      styleSrc: ["'self'", 'stackpath.bootstrapcdn.com', "'unsafe-inline'"],
+    },
+  })
+);
+
 // create typeorm connection
 createConnection().then((_) => {
   // Check for initial configuration and user
@@ -68,8 +80,16 @@ createConnection().then((_) => {
   // register routes
   app.post('/api/user/register', userController.register);
   app.post('/api/user/invite', jwtMiddleware.checkToken, userController.invite);
-  app.post('/api/user/email', jwtMiddleware.checkToken, userController.updateUserEmail);
-  app.post('/api/user/email/revoke', jwtMiddleware.checkToken, userController.revokeEmailRequest);
+  app.post(
+    '/api/user/email',
+    jwtMiddleware.checkToken,
+    userController.updateUserEmail
+  );
+  app.post(
+    '/api/user/email/revoke',
+    jwtMiddleware.checkToken,
+    userController.revokeEmailRequest
+  );
   app.post('/api/user/email/validate', userController.validateEmailRequest);
   app.patch('/api/user', jwtMiddleware.checkToken, userController.patch);
   app.get('/api/user', jwtMiddleware.checkToken, userController.getUser);
@@ -77,30 +97,118 @@ createConnection().then((_) => {
   app.get('/api/user/verify/:uuid', userController.verify);
   app.patch('/api/forgot-password', authController.forgotPassword);
   app.patch('/api/password-reset', authController.resetPassword);
-  app.post('/api/refresh', jwtMiddleware.checkRefreshToken, authController.refreshSession);
+  app.post(
+    '/api/refresh',
+    jwtMiddleware.checkRefreshToken,
+    authController.refreshSession
+  );
   app.patch('/api/user/password', userController.updateUserPassword);
   app.post('/api/login', authController.login);
-  app.post('/api/upload', jwtMiddleware.checkToken, fileUploadController.uploadFile);
-  app.get('/api/file/:id', jwtMiddleware.checkToken, fileUploadController.getFileById);
-  app.get('/api/organization', jwtMiddleware.checkToken, orgController.getActiveOrgs);
-  app.get('/api/organization/archive', jwtMiddleware.checkToken, orgController.getArchivedOrgs);
-  app.get('/api/organization/:id', jwtMiddleware.checkToken, orgController.getOrgById);
-  app.patch('/api/organization/:id/archive', jwtMiddleware.checkToken, orgController.archiveOrgById);
-  app.patch('/api/organization/:id/activate', jwtMiddleware.checkToken, orgController.activateOrgById);
-  app.patch('/api/organization/:id', jwtMiddleware.checkToken, orgController.updateOrgById);
-  app.post('/api/organization', jwtMiddleware.checkToken, orgController.createOrg);
-  app.get('/api/organization/asset/:id', jwtMiddleware.checkToken, assetController.getOrgAssets);
-  app.get('/api/organization/:id/asset/archive', jwtMiddleware.checkToken, assetController.getArchivedOrgAssets);
-  app.post('/api/organization/:id/asset', jwtMiddleware.checkToken, assetController.createAsset);
-  app.get('/api/organization/:id/asset/:assetId', jwtMiddleware.checkToken, assetController.getAssetById);
-  app.patch('/api/organization/:id/asset/:assetId', jwtMiddleware.checkToken, assetController.updateAssetById);
-  app.patch('/api/asset/archive/:assetId', jwtMiddleware.checkToken, assetController.archiveAssetById);
-  app.patch('/api/asset/activate/:assetId', jwtMiddleware.checkToken, assetController.activateAssetById);
-  app.delete('/api/asset/jira/:assetId', jwtMiddleware.checkToken, assetController.purgeJiraInfo);
-  app.get('/api/assessment/:id', jwtMiddleware.checkToken, assessmentController.getAssessmentsByAssetId);
-  app.get('/api/assessment/:id/vulnerability', jwtMiddleware.checkToken, assessmentController.getAssessmentVulns);
-  app.post('/api/assessment', jwtMiddleware.checkToken, assessmentController.createAssessment);
-  app.delete('/api/assessment/:assessmentId', jwtMiddleware.checkToken, assessmentController.deleteAssessmentById);
+  app.post(
+    '/api/upload',
+    jwtMiddleware.checkToken,
+    fileUploadController.uploadFile
+  );
+  app.get(
+    '/api/file/:id',
+    jwtMiddleware.checkToken,
+    fileUploadController.getFileById
+  );
+  app.get(
+    '/api/organization',
+    jwtMiddleware.checkToken,
+    orgController.getActiveOrgs
+  );
+  app.get(
+    '/api/organization/archive',
+    jwtMiddleware.checkToken,
+    orgController.getArchivedOrgs
+  );
+  app.get(
+    '/api/organization/:id',
+    jwtMiddleware.checkToken,
+    orgController.getOrgById
+  );
+  app.patch(
+    '/api/organization/:id/archive',
+    jwtMiddleware.checkToken,
+    orgController.archiveOrgById
+  );
+  app.patch(
+    '/api/organization/:id/activate',
+    jwtMiddleware.checkToken,
+    orgController.activateOrgById
+  );
+  app.patch(
+    '/api/organization/:id',
+    jwtMiddleware.checkToken,
+    orgController.updateOrgById
+  );
+  app.post(
+    '/api/organization',
+    jwtMiddleware.checkToken,
+    orgController.createOrg
+  );
+  app.get(
+    '/api/organization/asset/:id',
+    jwtMiddleware.checkToken,
+    assetController.getOrgAssets
+  );
+  app.get(
+    '/api/organization/:id/asset/archive',
+    jwtMiddleware.checkToken,
+    assetController.getArchivedOrgAssets
+  );
+  app.post(
+    '/api/organization/:id/asset',
+    jwtMiddleware.checkToken,
+    assetController.createAsset
+  );
+  app.get(
+    '/api/organization/:id/asset/:assetId',
+    jwtMiddleware.checkToken,
+    assetController.getAssetById
+  );
+  app.patch(
+    '/api/organization/:id/asset/:assetId',
+    jwtMiddleware.checkToken,
+    assetController.updateAssetById
+  );
+  app.patch(
+    '/api/asset/archive/:assetId',
+    jwtMiddleware.checkToken,
+    assetController.archiveAssetById
+  );
+  app.patch(
+    '/api/asset/activate/:assetId',
+    jwtMiddleware.checkToken,
+    assetController.activateAssetById
+  );
+  app.delete(
+    '/api/asset/jira/:assetId',
+    jwtMiddleware.checkToken,
+    assetController.purgeJiraInfo
+  );
+  app.get(
+    '/api/assessment/:id',
+    jwtMiddleware.checkToken,
+    assessmentController.getAssessmentsByAssetId
+  );
+  app.get(
+    '/api/assessment/:id/vulnerability',
+    jwtMiddleware.checkToken,
+    assessmentController.getAssessmentVulns
+  );
+  app.post(
+    '/api/assessment',
+    jwtMiddleware.checkToken,
+    assessmentController.createAssessment
+  );
+  app.delete(
+    '/api/assessment/:assessmentId',
+    jwtMiddleware.checkToken,
+    assessmentController.deleteAssessmentById
+  );
   app.get(
     '/api/asset/:assetId/assessment/:assessmentId',
     jwtMiddleware.checkToken,
@@ -117,11 +225,35 @@ createConnection().then((_) => {
     assessmentController.queryReportDataByAssessment
   );
   app.post('/api/report/generate', jwtMiddleware.checkToken, generateReport);
-  app.get('/api/vulnerability/:vulnId', jwtMiddleware.checkToken, vulnController.getVulnById);
-  app.delete('/api/vulnerability/:vulnId', jwtMiddleware.checkToken, vulnController.deleteVulnById);
-  app.patch('/api/vulnerability/:vulnId', jwtMiddleware.checkToken, vulnController.patchVulnById);
-  app.post('/api/vulnerability', jwtMiddleware.checkToken, vulnController.createVuln);
-  app.get('/api/vulnerability/jira/:vulnId', jwtMiddleware.checkToken, vulnController.exportToJira);
-  app.post('/api/config', jwtMiddleware.checkToken, configController.saveConfig);
+  app.get(
+    '/api/vulnerability/:vulnId',
+    jwtMiddleware.checkToken,
+    vulnController.getVulnById
+  );
+  app.delete(
+    '/api/vulnerability/:vulnId',
+    jwtMiddleware.checkToken,
+    vulnController.deleteVulnById
+  );
+  app.patch(
+    '/api/vulnerability/:vulnId',
+    jwtMiddleware.checkToken,
+    vulnController.patchVulnById
+  );
+  app.post(
+    '/api/vulnerability',
+    jwtMiddleware.checkToken,
+    vulnController.createVuln
+  );
+  app.get(
+    '/api/vulnerability/jira/:vulnId',
+    jwtMiddleware.checkToken,
+    vulnController.exportToJira
+  );
+  app.post(
+    '/api/config',
+    jwtMiddleware.checkToken,
+    configController.saveConfig
+  );
   app.get('/api/config', jwtMiddleware.checkToken, configController.getConfig);
 });
