58a549f397d54fab63352b20b426fdbb4439c090
Alejandro Saenz <alejandrosaenz117@gmail.com>
2021-02-06 00:22:42 -0500

    fix(docker-compose.yml): fix docker-compose bug
    fixed docker compose bug that did not allow angular to have an environment

--- src/init/setEnv.ts
+++ src/init/setEnv.ts
@@ -1,23 +1,9 @@
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const fs = require('fs');
 import * as path from 'path';
 const { writeFile } = require('fs');
 
-if (fs.existsSync(path.join(__dirname, '../../.env'))) {
-  const envPath = fs.readFileSync(path.join(__dirname, '../../.env'));
-  // tslint:disable-next-line: no-console
-  console.log('A .env file has been found found and will now be parsed.');
-  // https://github.com/motdotla/dotenv#what-happens-to-environment-variables-that-were-already-set
-  const envConfig = dotenv.parse(envPath);
-  if (envConfig) {
-    for (const key in envConfig) {
-      if (envConfig.hasOwnProperty(key)) {
-        process.env[key] = envConfig[key];
-      }
-    }
-    // tslint:disable-next-line: no-console
-    console.log('The provided .env file has been parsed successfully.');
-  }
+if (process.env.SERVER_ADDRESS && process.env.PORT) {
   let targetPath: string;
   let isProduction: boolean;
   const apiUrl = `${process.env.SERVER_ADDRESS}:${process.env.PORT}/api`;
@@ -39,23 +25,26 @@ if (fs.existsSync(path.join(__dirname, '../../.env'))) {
   // we have access to our environment variables
   // in the process.env object thanks to dotenv
   const environmentFileContent = `
-        export const environment = {
-           production: ${isProduction},
-           apiUrl: "${apiUrl}",
-        };
-        `;
+          export const environment = {
+             production: ${isProduction},
+             apiUrl: "${apiUrl}",
+          };
+          `;
 
   // write the content to the respective file
   writeFile(targetPath, environmentFileContent, (err) => {
     if (err) {
       console.error(err);
     }
-    console.info(`Wrote ${environmentFileContent} to ${targetPath}`);
+    // tslint:disable-next-line: no-console
+    console.info(
+      `Angular environment has been updated to ${environmentFileContent} located at ${targetPath}`
+    );
     return;
   });
 } else {
   // tslint:disable-next-line: no-console
   console.info(
-    'A .env file was not found. This is OK if running Docker. Attempting to fetch values from existing environment variables.'
+    'Environment variables do not exist.  Skipping `npm run confg` step!'
   );
 }
