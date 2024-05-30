382ce6a3ff8fd236aa06b9457d9981bd7e06c0f4
M4ON <ryan.mathus@cms.hhs.gov>
2024-05-30 16:08:51 -0400

    Various upgrades to packages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -331,7 +331,6 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {
       useHash: true,
-      relativeLinkResolution: 'legacy',
     }),
   ],
   exports: [RouterModule],
